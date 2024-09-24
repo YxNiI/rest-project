badd03c8f8bac4a51e7393bcad18ae6f8f2fdeac
Lyra Claus <l.claus@mce-bank.eu>
2024-09-24 11:35:37 +0200

    Initial commit.

---
+++ src/test/java/com/vocational_school/rest_project/RestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vocational_school.rest_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
